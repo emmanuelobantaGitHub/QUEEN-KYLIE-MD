871aa7bc9a140fa45e00b88da6094f0596b5f25e
Emma pelumi <149147592+emmanuelobantaGitHub@users.noreply.github.com>
2024-09-04 11:01:51 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put session id",
+SESSION_ID: process.env.SESSION_ID || "veRXECDK#y9C6M-xmQy3Zd_JaQ0NhwlUtB9VdWblN9t_BoMMhr7Q",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/198a322e8cc0f2bd6cba2.jpg",
-ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi QUEEN-KYLIE-MD Is Online Now 💻\n*💻 Owner* - QUEEN-KYLIE-MD\n\n*💻 Owner Number* -94765527900",
-SUDO_NB: process.env.SUDO_NB || "94765527900",
+ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi QUEEN-KYLIE-MD Is Online Now 💻\n*💻 Owner* - QUEEN-KYLIE-MD\n\n*💻 Owner Number* -234 912 618 9570",
+SUDO_NB: process.env.SUDO_NB || "234 912 618 9570",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE:"true"
